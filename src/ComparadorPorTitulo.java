d67ca7d5db5e6a9a63c626f33938dfb63ebcde87
mariandamblena <marianodambo@hotmail.com>
2025-07-13 00:49:07 -0300

    sumando commits

--- src/ComparadorPorTitulo.java
+++ src/ComparadorPorTitulo.java
@@ -6,4 +6,6 @@ public class ComparadorPorTitulo implements Comparator<Libro> {
     public int compare(Libro o1,Libro o2){
         return o1.getTitulo().compareToIgnoreCase(o2.getTitulo());
     }
+
 }
+
